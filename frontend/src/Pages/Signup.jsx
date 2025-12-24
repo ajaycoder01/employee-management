16fb34396570b1e1abc29458617d75c3808f9608
ajaycoder01 <ajaywebmaster01@gmail.com>
2025-12-25 02:18:52 +0530

    Update signup URL to production endpoint

--- frontend/src/Pages/Signup.jsx
+++ frontend/src/Pages/Signup.jsx
@@ -33,7 +33,7 @@ function Signup() {
     }
 
     try {
-      const url = "http://localhost:5000/auth/signup";
+      const url = "https://employee-management-backend-e17j.onrender.com/auth/signup";
       const response = await fetch(url,{
         method: "POST",
         headers:{
