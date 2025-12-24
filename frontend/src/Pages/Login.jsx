73ae4bc72758f39d32422c9f0502d03c058c2c6a
ajaycoder01 <ajaywebmaster01@gmail.com>
2025-12-25 02:17:58 +0530

    Update login API endpoint to production URL

--- frontend/src/Pages/Login.jsx
+++ frontend/src/Pages/Login.jsx
@@ -26,7 +26,7 @@ function Login() {
     }
 
     try {
-      const response = await fetch("http://localhost:5000/auth/login", {
+      const response = await fetch("https://employee-management-backend-e17j.onrender.com/auth/login", {
         method: "POST",
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(LoginInfo)
